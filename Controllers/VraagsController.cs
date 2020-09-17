c63cae71011c3a07d80e3df70d3fddfacb7ae274
FelixvL <digitaleservice@gmail.com>
2020-09-17 23:52:44 +0200

    Vraag Overzicht en Automatische invoer werkend

---
+++ Controllers/VraagsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DBExamenVragenAppDB;
+
+namespace WebExamenVragenAppYCAug.Controllers
+{
+    public class VraagsController : Controller
+    {
+        private DBExamContext db = new DBExamContext();
+
+        // GET: Vraags
+        public ActionResult Index()
+        {
+            return View(db.vragen.ToList());
+        }
+
+        // GET: Vraags/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vraag vraag = db.vragen.Find(id);
+            if (vraag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vraag);
+        }
+
+        // GET: Vraags/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vraags/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "VraagID,VraagTekst,Antwoord,Toelichting")] Vraag vraag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.vragen.Add(vraag);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vraag);
+        }
+
+        // GET: Vraags/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vraag vraag = db.vragen.Find(id);
+            if (vraag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vraag);
+        }
+
+        // POST: Vraags/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "VraagID,VraagTekst,Antwoord,Toelichting")] Vraag vraag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vraag).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vraag);
+        }
+
+        // GET: Vraags/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vraag vraag = db.vragen.Find(id);
+            if (vraag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vraag);
+        }
+
+        // POST: Vraags/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Vraag vraag = db.vragen.Find(id);
+            db.vragen.Remove(vraag);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebExamenVragenAppYCAug.MvcApplication" Language="C#" %>
