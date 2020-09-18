e4315fc87588e6336347efa2a73eb867b6363bb3
FelixvL <digitaleservice@gmail.com>
2020-09-18 14:51:50 +0200

    eigen controller met view opzetten, lijst gelukt

---
+++ Controllers/ZelfProberenController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DBExamenVragenAppDB;
+
+namespace WebExamenVragenAppYCAug.Controllers
+{
+    public class ZelfProberenController : Controller
+    {
+        private DBExamContext db = new DBExamContext();
+
+        // GET: Vraags
+        public ActionResult Index()
+        {
+            ViewBag.Huppakee = db.vragen.ToList();
+            return View(db.vragen.ToList());
+        }
+
+
+        public ActionResult CreeerVraag() {
+            return View();
+        }
+    }
+    
+}
