c69747e520cd936d118a0abe1ff638ae2e51952c
FelixvL <digitaleservice@gmail.com>
2020-09-24 13:10:34 +0200

    tijdens presentatie

--- Controllers/ZelfProberenController.cs
+++ Controllers/ZelfProberenController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,6 +23,31 @@ namespace WebExamenVragenAppYCAug.Controllers
         public ActionResult CreeerVraag() {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreeerVraag([Bind(Include = "VraagID,VraagTekst,Antwoord,Toelichting")] Vraag vraag) {
+            db.vragen.Add(vraag);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult WijzigVraag( int nummertje) {
+            Vraag vraag = db.vragen.Find(nummertje);
+            return View(vraag);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult WijzigVraag([Bind(Include = "VraagID,VraagTekst,Antwoord,Toelichting")] Vraag vraag) {
+            db.Entry(vraag).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult VerwijderVraag(int nummertje)
+        {
+            Vraag vraag = db.vragen.Find(nummertje);
+            db.vragen.Remove(vraag);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-    
+
 }
