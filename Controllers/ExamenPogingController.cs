ee7e69d28ddee29ff8f5251aaae46e448b7f5dcd
FelixvL <digitaleservice@gmail.com>
2020-09-25 14:47:26 +0200

    has a relatie vragen werkend volgende vorige slordig

--- Controllers/ExamenPogingController.cs
+++ Controllers/ExamenPogingController.cs
@@ -20,5 +20,32 @@ namespace WebExamenVragenAppYCAug.Controllers
 
             return View(db.vragen.ToList());
         }
+        public ActionResult StartExamen(int examennummer) {
+            Examen examen = db.examens.Find(examennummer);
+            return View(examen);
+        }
+        public ActionResult ToonVraag(int vraagindex, int examenid)
+        {
+            testDTO td = new testDTO();
+            td.huidigeIndex = vraagindex;
+            td.examen = db.examens.Find(examenid);
+            Console.WriteLine(td.examen.questions.Count + "<<<<<<") ;
+            td.huidigeVraag = td.examen.questions[vraagindex];
+            if (vraagindex > 0) {
+                td.vorigeVraag = td.examen.questions[vraagindex - 1];
+            }
+            if (vraagindex < td.examen.questions.Count) {
+                td.volgendeVraag = td.examen.questions[vraagindex + 1];
+            }
+            ViewBag.tdtd = td;
+            return View();
+        }
+    }
+    public class testDTO {
+        public Vraag huidigeVraag;
+        public Vraag vorigeVraag;
+        public Vraag volgendeVraag;
+        public Examen examen;
+        public int huidigeIndex;
     }
 }
