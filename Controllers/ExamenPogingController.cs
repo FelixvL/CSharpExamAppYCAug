b262fdd8225b1bc96d34c99b6b4aa1a3913e6296
FelixvL <digitaleservice@gmail.com>
2020-09-30 12:48:44 +0200

    poging en antwoord werkend

--- Controllers/ExamenPogingController.cs
+++ Controllers/ExamenPogingController.cs
@@ -22,13 +22,29 @@ namespace WebExamenVragenAppYCAug.Controllers
         }
         public ActionResult StartExamen(int examennummer) {
             Examen examen = db.examens.Find(examennummer);
+            ViewBag.pogingen = db.pogingen;
+            ViewBag.antwoorden = db.antwoorden;
             return View(examen);
         }
-        public ActionResult ToonVraag(int vraagindex, int examenid)
+        public ActionResult StartPoging(int examenid)
+        {
+            Examen examen = db.examens.Find(examenid);
+            Poging p = new Poging();
+            p.Examen = examen;
+            db.pogingen.Add(p);
+            db.SaveChanges();
+            ViewBag.pogingen = db.pogingen;
+            ViewBag.antwoorden = db.antwoorden;
+            return View("StartExamen",examen);
+        }
+        public ActionResult ToonVraag(int vraagindex, int pogingid)
         {
             testDTO td = new testDTO();
+            td.antwoord = new Antwoord();
             td.huidigeIndex = vraagindex;
-            td.examen = db.examens.Find(examenid);
+            Poging poging = db.pogingen.Find(pogingid);
+            td.poging = poging;
+            td.examen = db.examens.Find(poging.Examen.ExamenId);
             Console.WriteLine(td.examen.questions.Count + "<<<<<<") ;
             td.huidigeVraag = td.examen.questions[vraagindex];
             if (vraagindex > 0) {
@@ -38,7 +54,18 @@ namespace WebExamenVragenAppYCAug.Controllers
                 td.volgendeVraag = td.examen.questions[vraagindex + 1];
             }
             ViewBag.tdtd = td;
-            return View();
+            return View(td);
+        }
+        [HttpPost]
+        public String Hoi(string antwoord, int pogingid, int vraagid)
+        {
+            Antwoord mijnAntwoord = new Antwoord();
+            mijnAntwoord.AntwoordTekst = antwoord;
+            mijnAntwoord.Vraag = db.vragen.Find(vraagid);
+            mijnAntwoord.Poging = db.pogingen.Find(pogingid);
+            db.antwoorden.Add(mijnAntwoord);
+            db.SaveChanges();
+            return "is het deze" + antwoord + pogingid + vraagid;
         }
     }
     public class testDTO {
@@ -46,6 +73,8 @@ namespace WebExamenVragenAppYCAug.Controllers
         public Vraag vorigeVraag;
         public Vraag volgendeVraag;
         public Examen examen;
+        public Poging poging;
+        public Antwoord antwoord;
         public int huidigeIndex;
     }
 }
